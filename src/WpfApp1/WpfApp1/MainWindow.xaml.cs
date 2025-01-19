e938df4457f73005059a458285d66aa8fcab0392
gszentesi <szocir@gmail.com>
2025-01-19 08:45:06 +0100

    first run

---
+++ src/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            browser.ObjectForScripting = new ScriptHandler();
+
+            browser.Navigate(Path.GetFullPath(@"htmls\proba.html"));
+        }
+    }
+}
---
+++ src/WpfApp1/WpfApp1/ScriptHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public class ScriptHandler
+    {
+        public void SendMessage(string message)
+        {
+            MessageBox.Show($"Message from browser: {message}", "Message Received");
+        }
+    }
+}
